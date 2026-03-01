4942f2c5feb681953a86823b828ed6c479e362ae
Mounika-4960 <mouni.karanam.k@gmail.com>
2026-03-01 19:06:49 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from datetime import date
+import os
+
+st.title("🎉 Birthday & Special Events Dashboard")
+
+event_type = st.selectbox("Select Event Type", ["Birthday", "Anniversary", "Special Event"])
+name = st.text_input("Enter Name")
+event_date = st.date_input("Select Date")
+
+if st.button("Save Event"):
+    data = {"Name": name, "Event": event_type, "Date": event_date}
+    df = pd.DataFrame([data])
+
+    if os.path.exists("events.csv"):
+        old_df = pd.read_csv("events.csv")
+        df = pd.concat([old_df, df], ignore_index=True)
+
+    df.to_csv("events.csv", index=False)
+    st.success("Event Saved Successfully!")
+
+st.write("----")
+st.subheader("All Events")
+
+if os.path.exists("events.csv"):
+    df = pd.read_csv("events.csv")
+    st.dataframe(df)
